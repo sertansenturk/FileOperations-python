08dcbcfbe8e6232acc3b3d9ce4dde8f07ba87906
Sertan Senturk <contact@sertansenturk.com>
2015-09-21 01:48:23 +0200

    Minor improvements

--- fileoperations/fileoperations.py
+++ fileoperations/fileoperations.py
@@ -1,22 +1,33 @@
 import os
 
-def getFileNamesInDir(dir_name, audio_ext = '.mp3', skip_foldername = ''):
+def getFileNamesInDir(dir_name, extension = '.mp3', skip_foldername = '', 
+					verbose = False):
 	names = []
 	folders = []
 	fullnames = []
-	print dir_name
 
+	if verbose:
+		print dir_name
+
+	# check if the folder exists
 	if not os.path.isdir(dir_name):
-		print 'Directory doesn\'t exist!'
+		if verbose:
+			print "> Directory doesn't exist!"
 		return [], [], []
-	dir_name = dir_name[:-1] if dir_name[-1] == '/' else dir_name
+
+	# if the dir_name finishes with the file separator, 
+	# remove it so os.walk works properly
+	dir_name = dir_name[:-1] if dir_name[-1] == os.sep else dir_name
+
+	# walk all the subdirectories
 	for (path, dirs, files) in os.walk(dir_name):
 		for f in files:
-			if f.lower()[-len(audio_ext):] == audio_ext:
+			if f.lower()[-len(extension):] == extension:
 				if skip_foldername not in path.split(os.sep)[1:]:
 					folders.append(unicode(path, 'utf-8'))
 					names.append(unicode(f,'utf-8'))
 					fullnames.append(os.path.join(path,f))
 
-	print "> Found " + str(len(names)) + " files."
+	if verbose:
+		print "> Found " + str(len(names)) + " files."
 	return fullnames, folders, names
