cd55b36666065c0ea21c45cae0bc5fc6eef96737
Sertan Senturk <contact@sertansenturk.com>
2016-01-25 01:49:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name='fileoperations_python',
-      version='0.2',
+      version='1.0',
       description='Basic I/O stuff for python',
       author='Sertan Senturk',
+      license='agpl 3.0',
       author_email='contact@sertansenturk.com',
       url='https://github.com/sertansenturk/fileoperations_python',
       packages=find_packages(exclude=["test"])
