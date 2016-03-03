64786db01261f0cb403813a3bd7e575696b8704e
Sertan Senturk <contact@sertansenturk.com>
2016-03-03 11:13:06 +0100

    Refactored fileoperations according to PEP8

--- fileoperations/fileoperations.py
+++ fileoperations/fileoperations.py
@@ -1,35 +1,42 @@
 import os
 import fnmatch
 
-def getFileNamesInDir(dir_name, keyword = '*.mp3', skip_foldername = '', 
-					match_case = True, verbose = None):
-	names = []
-	folders = []
-	fullnames = []
 
-	if verbose:
-		print dir_name
+def get_filenames_in_dir(dir_name, keyword='*.mp3', skip_foldername='',
+                         match_case=True, verbose=None):
+    names = []
+    folders = []
+    fullnames = []
 
-	# check if the folder exists
-	if not os.path.isdir(dir_name):
-		if verbose:
-			print "> Directory doesn't exist!"
-		return [], [], []
+    if verbose:
+        print dir_name
 
-	# if the dir_name finishes with the file separator, 
-	# remove it so os.walk works properly
-	dir_name = dir_name[:-1] if dir_name[-1] == os.sep else dir_name
+    # check if the folder exists
+    if not os.path.isdir(dir_name):
+        if verbose:
+            print "> Directory doesn't exist!"
+        return [], [], []
 
-	# walk all the subdirectories
-	for (path, dirs, files) in os.walk(dir_name):
-		for f in files:
-			hasKey = (fnmatch.fnmatch(f, keyword) if match_case else 
-				fnmatch.fnmatch(f.lower(), keyword.lower()))
-			if hasKey and skip_foldername not in path.split(os.sep)[1:]:
-				folders.append(unicode(path, 'utf-8'))
-				names.append(unicode(f,'utf-8'))
-				fullnames.append(os.path.join(path,f))
+    # if the dir_name finishes with the file separator,
+    # remove it so os.walk works properly
+    dir_name = dir_name[:-1] if dir_name[-1] == os.sep else dir_name
 
-	if verbose:
-		print "> Found " + str(len(names)) + " files."
-	return fullnames, folders, names
+    # walk all the subdirectories
+    for (path, dirs, files) in os.walk(dir_name):
+        for f in files:
+            hasKey = (fnmatch.fnmatch(f, keyword) if match_case else
+                      fnmatch.fnmatch(f.lower(), keyword.lower()))
+            if hasKey and skip_foldername not in path.split(os.sep)[1:]:
+                folders.append(unicode(path, 'utf-8'))
+                names.append(unicode(f, 'utf-8'))
+                fullnames.append(os.path.join(path, f))
+
+    if verbose:
+        print "> Found " + str(len(names)) + " files."
+    return fullnames, folders, names
+
+# alias function for backwards compatibility
+def getFileNamesInDir(dir_name, keyword='*.mp3', skip_foldername='',
+                      match_case=True, verbose=None):
+    get_filenames_in_dir(dir_name, keyword=keyword, skip_foldername=skip_foldername,
+                         match_case=match_case, verbose=verbose)
